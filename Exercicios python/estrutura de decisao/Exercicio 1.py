ffc69858395f40a8c5eee4efa297144217127527
rpaschoaljr <r.paschoal@hotmail.com>
2022-01-18 20:21:29 -0300

    Exercicio

---
+++ Exercicios python/estrutura de decisao/Exercicio 1.py
@@ -0,0 +1,6 @@
+primeiro_numero = float(input('DIgite um numero: '))
+segundo_numero = float(input('DIgite outro numero: '))
+if primeiro_numero > segundo_numero:
+    print("O %.2f e maior que o %.2f" %(primeiro_numero, segundo_numero))
+else:
+    print("O %.2f e maior que o %.2f" %(segundo_numero, primeiro_numero))
